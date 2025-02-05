462718ac524cb238d35ffafc459f2ded5532d093
yejianbin233 <877811992@qq.com>
2025-02-05 23:25:32 +0800

    InfiniteGame Init

---
+++ Source/InfiniteGame/InfiniteGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "InfiniteGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, InfiniteGame, "InfiniteGame" );
